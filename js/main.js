c1936a5b7aa6ea54514d52bf3379eeecc701d438
Garg-har-06 <gargharshita624@gmail.com>
2022-05-03 14:54:24 +0530

    responsive

--- js/main.js
+++ js/main.js
@@ -95,4 +95,12 @@
             section.classList.add("hide");
         }
     })
-})();
+})(); 
+
+window.addEventListener("load",() =>{
+    //preloader
+    document.querySelector(".preloader").classList.add("fade-out");
+    setTimeout(() =>{
+        document.querySelector(".preloader").style.display="none";
+    },600)
+})
